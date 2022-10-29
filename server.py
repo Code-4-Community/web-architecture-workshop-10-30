da60e2203e3ddf80f3d27e9b4327918f9b6db168
Varun Thakkar <varunthakkar2013@gmail.com>
2022-10-29 18:49:38 -0400

    finish up routes

--- server.py
+++ server.py
@@ -1,44 +1,62 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
+next_post_id: int = 0
 app = FastAPI()
-db = {"accounts": [],  "posts": []}
+db = {"posts": []}
 
+class Post(BaseModel):
+    title: str
+    body: str
 
 @app.get("/")
 async def root():
     return {"message": "This is the C4C Backend Workshop Backend!"}
 
-# ********************************** ACCOUNTS **********************************************
-
-
-@app.get("/accounts")
-@app.post("/accounts")
-def add_account():
-
-
-@app.delete("/accounts/{id}")
-def delete_account(account_id):
-    accounts = db['accounts']
-    for account in accounts:
-        if account_id == account['id']:
-            db['accounts'].remove(account)
-    raise HTTPException(status_code=404, detail="Item not found")
-
-
-# ********************************** SPECIFIC ACCOUNT **********************************************
-
-@app.get("/accounts/{id}")
-def get_account(account_id):
-    accounts = db['accounts']
-    for account in accounts:
-        if account_id == account['id']:
-            return account
-    raise HTTPException(status_code=404, detail="Item not found")
-
-
-@app.post("/accounts/{id}")
-# ********************************** POSTS **********************************************
+# Returns all posts
 @app.get("/posts")
-# EXERCISE
-@app.put("/posts")
-@app.delete("posts")
+def get_posts():
+    return db['posts']
+
+# Creates a new entry for Post
+# Will raise exception if body parameters are invalid
+@app.post("/posts")
+def add_post(new_post: Post):
+    if (new_post.title.strip() == '' or new_post.body.strip() == ''):
+        raise HTTPException(status_code=400, detail="Invalid input")
+    else:
+        global next_post_id
+        id = next_post_id
+        db['posts'].append({ 'id': id, 'title': new_post.title, 'body': new_post.body })
+        next_post_id += 1
+        return id
+
+# Removes a post from the 'database'
+@app.delete("/posts/{post_id}")
+def delete_post(post_id: int):
+    posts = db['posts']
+    for post in posts:
+        if post_id == post['id']:
+            db['posts'].remove(post)
+            return 'OK'
+    raise HTTPException(status_code=404, detail="Item not found")
+
+# Retrieves a single post from the 'database'
+@app.get("/posts/{post_id}")
+def get_post(post_id: int):
+    posts = db['posts']
+    for post in posts:
+        if post_id == post['id']:
+            return post
+    raise HTTPException(status_code=404, detail="Item not found")
+
+# Exercises
+# Updates a post in the 'database'
+@app.put("/posts/{post_id}")
+def update_post(post_id: int, updated_post: Post):
+    pass
+
+# Deletes all posts from the 'database'
+@app.delete("/posts")
+def delete_all_posts():
+    pass
